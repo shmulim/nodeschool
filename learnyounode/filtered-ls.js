040fafac690fdf03bbcff46a1c4866921d1efdfe
shmulim <shmuel.markel@gmail.com>
2015-06-10 23:20:24 -0700

    filtered ls

---
+++ learnyounode/filtered-ls.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+fs.readdir(process.argv[2], function(err, files){
+		files.forEach(function(i){
+			if (i.indexOf('.' + process.argv[3]) > -1){
+			console.log(i);
+		}
+	});
+});
