1e4d23472f45bd7425700c8b77f3e57b2ddd924b
@shmulim <shmuel.markel@gmail.com>
2015-06-11 20:27:46 -0700

    Update httpget.js

--- learnyounode/httpget.js
+++ learnyounode/httpget.js
@@ -1,16 +1,7 @@
-var http = require('http');
-
-http.get(process.argv[2], function(response){
-	var arr = [];
-
-	response.setEncoding('utf8');
-	response.on('data', function(chunk){
-		arr.push(chunk);
-	});
-
-	response.on('end', function(){
-		arr.forEach(function(i){
-			console.log(i);
-		});
-	});
-});
+    var http = require('http');
+    
+    http.get(process.argv[2], function (response) {
+      response.setEncoding('utf8');
+      response.on('data', console.log);
+      response.on('error', console.error);
+    });
